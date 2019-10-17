8b9fcfde317a05765bf3eece29bf7bbaead52040
Kevin Tran <kevin115533@yahoo.com>
2019-10-17 14:37:32 -0600

    Weekly lab
    writing to file works

--- venv/fileNavigator.py
+++ venv/fileNavigator.py
@@ -6,8 +6,12 @@ This program allows a user to input lines of text into a file and then navigate 
 def main():
     fileName = input("Enter the name of your file: ")
     file = open(fileName + ".txt", 'w')
-
-
-
+    wordList = []
+    word = input("Enter a string or hit Enter to quit: ")
+    while word != "":
+        wordList = wordList + [word]
+        word = input("Enter a string or hit Enter to quit: ")
+    for x in range(len(wordList)):
+        file.write(wordList[x] + "\n")
 main()
 
